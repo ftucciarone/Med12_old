288b17f62fa0c27be8ade83fe3c7346eac057bfd
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-07 16:20:03 +0100

    Create create_dirtree.sh

---
+++ preprocessing-era5/create_dirtree.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+run_dir=run
+grib_dir=grib
+work_dir=work
+arch_dir=archive
+figs_dir=figures
+
+mkdir -p $run_dir
+mkdir -p $grib_dir
+mkdir -p $work_dir
+mkdir -p $arch_dir
+mkdir -p $figs_dir
