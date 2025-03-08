eefd95e5d13378cfdfd7d7894c4594f5c8a85f6d
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-08 16:59:23 +0100

    Update compile_fillLand.sh

--- preprocessing-era5/tools/compile_fillLand.sh
+++ preprocessing-era5/tools/compile_fillLand.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+#set -x
 
 COMP="ifort"
 NCDF=/opt/intel/netcdf-c-4.7.4-f-4.5.3-impi
@@ -15,10 +15,15 @@ ADD=""
 #  --flibs     -> -L/usr/local/Cellar/netcdf-fortran/4.6.1/lib -lnetcdff
 #
 #
-nc_config__fflags="-I/usr/include "
-nc_config__flibs="-L/usr/lib/x86_64-linux-gnu -lnetcdff -Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -lnetcdf -lnetcdf -ldl -lm"
 
-COMP="gfortran -Ofast -fbounds-check -Wall -Wno-uninitialized -ffree-line-length-512 "
+nc_config__which=/usr/bin/nc-config
+nc_config__which=$( which nc-config ) || { echo "which nc-config failed: check syntax or specify directly the path."; exit ; }
+nc_config__fflags=$( $nc_config__which --fflags ) || { echo "nc-config --flags failed: check syntax."; exit ; }
+nc_config__flibs=$( $nc_config__which --flibs) || { echo "nc-config --flibs failed:check syntax."; exit ; }
+nc_config__fc=$( $nc_config__which --fc) || { echo "nc-config --fc failed:check syntax or specify the compiler."; exit ; }
+
+
+COMP="$nc_config__fc -Ofast -fbounds-check -Wall -Wno-uninitialized -ffree-line-length-512 "
 
 str=""
 for f in master_flandr.F90 read_field2dt_fillLand.F90 read_msk.F90 handlerr.F90 write_field2dt_fillLand.F90 fill_land.F90 ; do
